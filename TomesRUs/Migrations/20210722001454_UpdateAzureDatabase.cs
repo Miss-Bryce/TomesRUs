0c2755f990767712ed17ec751cc8c6fcd520e7be
Jillian Bryce <200440217@student.georgianc.on.ca>
2021-07-21 20:16:10 -0400

    Azure implemented database

---
+++ TomesRUs/Migrations/20210722001454_UpdateAzureDatabase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TomesRUs.Migrations
+{
+    public partial class UpdateAzureDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
